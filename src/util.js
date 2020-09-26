ba9eccf0795ac2761c80b52954c0a4f3d3fe4a4b
sherjeelaq <55728493+sherjeelaq@users.noreply.github.com>
2020-09-26 15:48:15 +0500

    Add files via upload

---
+++ src/util.js
@@ -0,0 +1,59 @@
+import { Circle, Popup } from "react-leaflet";
+import React from "react";
+import numeral from "numeral";
+
+const casesTypeColors = {
+  cases: {
+    hex: "#1976d2",
+    multiplier: 800,
+  },
+  recovered: {
+    hex: "#4caf50",
+    multiplier: 1200,
+  },
+  deaths: {
+    hex: "#fb4443",
+    multiplier: 2000,
+  },
+};
+
+export const sortData = (data) => {
+  const sortData = [...data];
+  return sortData.sort((a, b) => (a.cases > b.cases ? -1 : 1));
+};
+
+export const prettyPrintStat = (stat) =>
+  stat ? `+${numeral(stat).format("0.0a")}` : "+0";
+
+export const showDataOnMap = (data, casesType = "cases") =>
+  data.map((country) => (
+    <Circle
+      center={[country.countryInfo.lat, country.countryInfo.long]}
+      color={casesTypeColors[casesType].hex}
+      fillColor={casesTypeColors[casesType].hex}
+      fillOpacity={0.4}
+      radius={
+        (Math.sqrt(country[casesType]) / 1.4) *
+        casesTypeColors[casesType].multiplier
+      }
+    >
+      <Popup>
+        <div className="info-container">
+          <div
+            className="info-flag"
+            style={{ backgroundImage: `url(${country.countryInfo.flag})` }}
+          ></div>
+          <div className="info-name">{country.country}</div>
+          <div className="info-confirmed">
+            Cases: {numeral(country.cases).format("0,0")}
+          </div>
+          <div className="info-recovered">
+            Recovered: {numeral(country.recovered).format("0,0")}
+          </div>
+          <div className="info-deaths">
+            Deaths: {numeral(country.deaths).format("0,0")}
+          </div>
+        </div>
+      </Popup>
+    </Circle>
+  ));
